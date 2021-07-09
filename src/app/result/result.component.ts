3401ac5d5373a14bcde5ad59bdb69456350eb290
lasha-TS <lashatsevelidzelts@gmail.com>
2021-07-09 14:18:07 +0400

    new

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -8,11 +8,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultComponent implements OnInit {
   successAnswer: any;
+  text?: string;
 
   constructor(private Route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.successAnswer = this.Route.snapshot.queryParams.count;
+    if(this.successAnswer <= 5){
+      this.text = `Your score: ${this.successAnswer}. The result is low`
+    }else if(this.successAnswer > 5 && this.successAnswer < 10){
+      this.text = `Your score: ${this.successAnswer}. You can do more`
+    }else{
+      this.text = `Your score: ${this.successAnswer}. Excellent`
+    }
   }
 
 
